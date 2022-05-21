9280f70a7a1a1b11b31ce7d11c2b63558bd2e736
Florian Dubois <26360935+FlorianLeChat@users.noreply.github.com>
2022-05-21 21:01:15 +0200

    Amélioration du formatage des scripts JS

--- javascript/aproposdemoi.js
+++ javascript/aproposdemoi.js
@@ -1,44 +1,38 @@
-let sources = [ "../image/fille_1", "../image/hibou", 
-                "../image/fille_fleur"];
-
-
-
 let indice = 0;
+let sources = [
+    "image/fille_1",
+    "image/hibou",
+    "image/fille_fleur"
+];
 
-function next() {
-    indice = (indice + 1) % sources.length;
+let indice_2 = 0;
+let sources_2 = [
+    "image/st_jean_criques_du_rouvier",
+    "image/nancy_stanislas_3",
+    "image/portugal_pont",
+    "image/nice_massena"
+];
+
+let indice_3 = 0;
+let sources_3 = [
+    "image/livre_viking",
+    "image/livre_nordique"
+];
+
+function next()
+{
+    indice = ( indice + 1 ) % sources.length;
     document.querySelector( "#show" ).src = sources[ indice ];
 }
 
-
-
-let sources_2 = [ "../image/st_jean_criques_du_rouvier", "../image/nancy_stanislas_3", 
-                    "../image/portugal_pont", "../image/nice_massena"
-              ];
-
-
-
-let indice_2 = 0;
-
-function next_2() {
-    indice_2 = (indice_2 + 1) % sources_2.length;
+function next_2()
+{
+    indice_2 = ( indice_2 + 1 ) % sources_2.length;
     document.querySelector( "#show_2" ).src = sources_2[ indice_2 ];
 }
 
-let sources_3 = [ "../image/livre_viking", "../image/livre_nordique"
-              ];
-
-
-
-let indice_3 = 0;
-
-function next_3() {
-    indice_3 = (indice_3 + 1) % sources_3.length;
+function next_3()
+{
+    indice_3 = ( indice_3 + 1 ) % sources_3.length;
     document.querySelector( "#show_3" ).src = sources_3[ indice_3 ];
 }
-
-
-
-
-
-
