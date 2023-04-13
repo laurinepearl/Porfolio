d74cb49eda29d0f5077727a549af972e546aa77d
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-13 13:59:32 +0200

    Suppression de la fonction anonyme pour les écouteurs JS

--- scripts.js
+++ scripts.js
@@ -1,23 +1,18 @@
-function toggleMenu()
-{
-	const navbar = document.querySelector( ".navbar" );
-	const burger = document.querySelector( ".burger" );
+const navbar = document.querySelector( ".navbar" );
+const burger = document.querySelector( ".burger" );
 
-	burger.addEventListener( "click", ( _event ) =>
+burger.addEventListener( "click", ( _event ) =>
+{
+	navbar.classList.toggle( "show-nav" );
+} );
+
+// bonus
+const navbarLinks = document.querySelectorAll( ".navbar a" );
+
+navbarLinks.forEach( link =>
+{
+	link.addEventListener( "click", ( _event ) =>
 	{
 		navbar.classList.toggle( "show-nav" );
 	} );
-
-	// bonus
-	const navbarLinks = document.querySelectorAll( ".navbar a" );
-
-	navbarLinks.forEach( link =>
-	{
-		link.addEventListener( "click", ( _event ) =>
-		{
-			navbar.classList.toggle( "show-nav" );
-		} );
-	} );
-}
-
-toggleMenu();
+} );
