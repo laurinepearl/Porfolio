68ecd5567decb80351d7c3e2165494927164579e
Laurine Ferreira <98741654+laurinepearl@users.noreply.github.com>
2023-12-11 21:07:35 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,3 +16,28 @@ navbarLinks.forEach( link =>
 		navbar.classList.toggle( "show-nav" );
 	} );
 } );
+
+let currentIndex = 0;
+
+function showSlide(index) {
+  const track = document.getElementById("carouselTrack");
+  const slides = document.querySelectorAll(".carousel-slide");
+
+  if (index < 0) {
+    index = slides.length - 1;
+  } else if (index >= slides.length) {
+    index = 0;
+  }
+
+  const translationValue = -index * (100 / slides.length) + "%";
+  track.style.transform = "translateX(" + translationValue + ")";
+  currentIndex = index;
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
