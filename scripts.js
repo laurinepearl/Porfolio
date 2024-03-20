24f94ef4d5f09f93b295326161e5361d410bd1c5
Laurine Ferreira <98741654+laurinepearl@users.noreply.github.com>
2024-03-20 20:43:07 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -43,3 +43,18 @@ function nextSlide() {
   showSlide(currentIndex + slidesToShow);
 }
 
+ // Bouton de retour en haut de la page
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollToTopBtn").style.display = "none";
+    }
+}
+
+document.getElementById("scrollToTopBtn").addEventListener("click", function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
